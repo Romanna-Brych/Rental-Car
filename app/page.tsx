86b2a9843658270255596fd8b2f3d2708727517d
Romanna <romannabric@gmail.com>
2026-03-09 19:10:52 +0100

    implement home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <p>Home</p>
+        <h1>Find your perfect rental car</h1>
+        <h2>Reliable and budget-friendly rentals for any journey</h2>
+        <Link href="/catalog">View Catalog</Link>
       </main>
     </div>
   );
