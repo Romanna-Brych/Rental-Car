d148d38bd5359204aca4c326432205b50bee9b7b
Romanna <romannabric@gmail.com>
2026-03-09 21:43:44 +0100

    add cars API

---
+++ lib/api.ts
@@ -0,0 +1,24 @@
+import { CarsResponse } from '@/types/car';
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://car-rental-api.goit.global',
+});
+
+interface CarsQueryParams {
+  brand?: string;
+  rentalPrice?: string;
+  minMileage?: string;
+  maxMileage?: string;
+  limit?: string;
+  page?: string;
+}
+
+export async function getCars(
+  params: CarsQueryParams = {}
+): Promise<CarsResponse> {
+  const { data } = await api.get<CarsResponse>('/cars', {
+    params,
+  });
+  return data;
+}
