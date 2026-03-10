b9dc427aa1cb2fb22b37cefe27c1d765e9803457
Romanna <romannabric@gmail.com>
2026-03-10 17:55:21 +0100

    add components for catalog page

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,5 +1,10 @@
-function CatalogPage() {
-  return <p>Catalog</p>;
+import CarList from '@/components/CarList/CarList';
+import { getCars } from '@/lib/api';
+
+async function CatalogPage() {
+  const data = await getCars();
+  console.log(data.cars);
+  return <CarList cars={data.cars} />;
 }
 
 export default CatalogPage;
---
+++ components/CarCard/CarCard.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Car } from '@/types/car';
+import css from './CarCard.module.css';
+
+interface CarCardProps {
+  car: Car;
+}
+
+function CarCard({ car }: CarCardProps) {
+  const [, city, country] = car.address.split(', ');
+  const mileage = car.mileage.toLocaleString('uk-UA');
+  return (
+    <li className={css.card}>
+      <Image
+        className={css.img}
+        src={car.img}
+        alt={`${car.brand} ${car.model}`}
+        width={268}
+        height={276}
+      />
+      <div className={css.info}>
+        <div className={css.titleRow}>
+          <p className={css.title}>
+            {car.brand} <span className={css.model}>{car.model}</span>,
+            {car.year}
+          </p>
+          <p className={css.price}>${car.rentalPrice}</p>
+        </div>
+        <p className={css.details}>
+          {city} | {country} | {car.rentalCompany} | {car.type} | {mileage}
+          km
+        </p>
+      </div>
+      <Link className={css.btn} href={`/catalog/${car.id}`}>
+        Read more
+      </Link>
+    </li>
+  );
+}
+
+export default CarCard;
