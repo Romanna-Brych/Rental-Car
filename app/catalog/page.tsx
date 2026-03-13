306faae66e729aba5f70fe8242c5ac0484922018
Romanna <romannabric@gmail.com>
2026-03-13 21:34:04 +0100

    update prefetch and css

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -4,15 +4,30 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import { getBrands } from '@/lib/api';
+import { getBrands, getCars } from '@/lib/api';
+import { defaultFilters } from '@/lib/store/carsStore';
 
 export default async function CatalogPage() {
   const queryClient = new QueryClient();
+  const initialPage = 1;
+  const limit = 12;
 
-  await queryClient.prefetchQuery({
-    queryKey: ['brands'],
-    queryFn: getBrands,
-  });
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ['brands'],
+      queryFn: getBrands,
+    }),
+    queryClient.prefetchInfiniteQuery({
+      queryKey: ['cars', defaultFilters],
+      queryFn: ({ pageParam = initialPage }) =>
+        getCars({
+          ...defaultFilters,
+          page: String(pageParam),
+          limit: String(limit),
+        }),
+      initialPageParam: initialPage,
+    }),
+  ]);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
